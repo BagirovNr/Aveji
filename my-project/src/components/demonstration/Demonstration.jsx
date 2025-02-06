7246c340f75ee9204ad30b4131e2ee83f61b6e97
Nadir <nadir.bagirov2000@gmail.com>
2025-02-06 21:52:09 +0400

    :fire: add new component

--- my-project/src/components/demonstration/Demonstration.jsx
+++ my-project/src/components/demonstration/Demonstration.jsx
@@ -4,21 +4,21 @@ function Demonstration() {
   return (
     <>
     <div className='flex justify-between items-end'>
-        <p className='font-semibold text-7xl leading-[86px] w-[794px] text-[#212529]'>Эксклюзивная и нестандартная мебель для дома</p>
+        <p className='font-semibold text-7xl leading-[86px] w-[794px] text-[#212529]'>Эксклюзивная и нестандартная мебель для дома</p>
         <p className='font-normal text-lg leading-5 border-b'>ЗАКАЗАТЬ ПРОЕКТ</p>
     </div>
 
 <div className='flex justify-between'>
     <div className='flex flex-col gap-60'>
 <div >
-<p className='w-[445px] leading-7 text-xl font-normal'>Мы можем произвести любую мебель для вашего проекта и найти производственное решение любой задумки.</p>
+<p className='w-[445px] leading-7 text-xl font-normal'>Мы можем произвести любую мебель для вашего проекта и найти производственное решение любой задумки.</p>
 </div>
 <div>
     <img src={smfoto} alt="" />
 </div>
     </div>
     <div>
-        <img src={space} alt="" />
+        <img className='max-w-fit' src={space} alt="" />
     </div>
 </div>
     </>
